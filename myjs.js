45955b54b575cedc2df2a1b8cce2c86619eae548
zm25141 <zm25141@josai.ac.jp>
2025-10-14 10:21:00 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,8 +1,9 @@
-alert{'Hello, Kosei!'};
-alert{1+1};
-alert{Math.sprt{1+1}};
-alert{2+3};
-alert{'2'+'3'};
-alert{Number{'2'}+Number{'3'}};
-alert{'2'+3};
-alert{{'2'}+3}
+alert('Hello, Kosei!');
+alert(1+1);
+alert(Math.sprt(1+1));
+alert(Math.sprt(2+3));
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert(('2')+3);
+alert(Number('2')+3);
