23066ec110c0307a1967dc0581c3b5eb806712cd
zm25141 <zm25141@josai.ac.jp>
2025-11-25 10:28:30 +0900

    Create ex6-9.js

---
+++ ex6-9.js
@@ -0,0 +1,10 @@
+function sayhello69(){
+   alert('Hello,' + document.getElementById('who69').innerHTML + 
+'!');
+}
+function taro69(){
+  document.getElementById('who69').innerHTML = '太郎';
+}
+function hanako69(){
+  document.getElementById('who69').innerHTML = '花子';
+}
