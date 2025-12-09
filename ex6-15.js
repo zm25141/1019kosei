f001e4028645cd118a266bab047d7926942c50e3
zm25141 <zm25141@josai.ac.jp>
2025-12-09 10:23:58 +0900

    Create ex6-15.js

---
+++ ex6-15.js
@@ -0,0 +1,15 @@
+function hyoji(){
+  //表示する文字
+var str="一文字ずつ表示します。";
+  //テキストボックスの文字数
+var cnt=document.timer.moji.value.length;
+  //文字が全部表示されているか確認
+if (cnt<11){
+  //現在より1文字多く切り出して表示
+  document..timer.moji.value=str.substr(0,cnt+1);}
+  else{
+    //すべて表示されたら、空文字に戻す
+  document.timer.moji.value="";}}
+function startfnc(){
+  //関数hyouji()を1000ミリ秒間隔で呼び出す
+setInterval("hyoji()",1000);}
