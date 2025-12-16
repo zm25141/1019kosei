ebee410bbedc78cb6619c243bea19924c2b2d9de
zm25141 <zm25141@josai.ac.jp>
2025-12-16 09:49:18 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,4 +1,4 @@
 function gazo610()
 {document.getElementById('who610').innerHTML =
-  '<image src="./image/download.jpg" alt="すき焼き" width="200">' 
+  '<img src="./image/download.jpg" alt="すき焼き" width="200">' 
   ;}
