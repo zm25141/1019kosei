ce8b294cbe184f7ad093ca87df2dd887b3e09997
zm25141 <zm25141@josai.ac.jp>
2026-01-06 10:29:19 +0900

    Update ex6-17.js

--- ex6-17.js
+++ ex6-17.js
@@ -4,8 +4,10 @@ var str17="一文字ずつ表示します。";
   //テキストボックスの文字数
 var cnt17=document.timer17.moji17.value.length;
 //文字が全部表示されているか確認
-  if(cnt17<11){
+  if (cnt17<11){
     //現在より１文字多く切り出して表示
+    document.timer17.moji17.value=str17.substr(0,cnt17+1);}else{
+    //すべて表示されたら、空文字に戻す
     document.timer17.moji17.value="";}
   //setTimeout()を含む関数を呼び出す
   setTimeout("hyoji17()",1000);}
