b9c76f7bb878bc689febcfb232815e74fc233140
zm25141 <zm25141@josai.ac.jp>
2026-01-31 20:14:50 +0900

    Create ex6-18.js

---
+++ ex6-18.js
@@ -0,0 +1,11 @@
+function changePhoto618() {
+  document.getElementById("myPhoto2").src =
+    "image/WIN_20260128_21_29_25_Pro.jpg";
+
+  setTimeout(returnPhoto618, 3000);
+}
+
+function returnPhoto618() {
+  document.getElementById("myPhoto2").src =
+    "image/WIN_20260128_21_29_27_Pro.jpg";
+}
