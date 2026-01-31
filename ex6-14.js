2495904856295bf00729a44d00f51753df590444
zm25141 <zm25141@josai.ac.jp>
2026-01-31 21:54:49 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -3,7 +3,7 @@ function changePhoto614() {
   const myImage = document.getElementById("myPhoto1");
   if (!myImage) return;
     // '56.jpg' の画像パス (フォルダ名 'Image' は大文字)
-    myImage.src = "image/WIN_20260128_21_45_56_Pro.jpg";
+    myImage.src = "images/awwHokBK_400x400.png";
   }
 
 function returnPhoto614() {
@@ -11,5 +11,5 @@ function returnPhoto614() {
   const myImage = document.getElementById("myPhoto1");
   if (!myImage) return;
     // '54.jpg' の画像パス (フォルダ名 'image' は小文字)
-    myImage.src = "image/WIN_20260128_21_45_54_Pro.jpg";
+    myImage.src = "images/awwHokBK_400x400.png";
   }
